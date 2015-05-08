91f03c39bb8e5142a4aa7cb7281486981eef02a4
Hugo Klepsch <hugo.klepsch@gmail.com>
2015-05-08 14:49:42 -0400

    Commented genericArray

--- Stacks vs Queues/src/stackQueueGenericTObj/genericArray.java
+++ Stacks vs Queues/src/stackQueueGenericTObj/genericArray.java
@@ -2,7 +2,7 @@
 		 Title: genericArray.java
 		 Programmer: hugo
 		 Date of creation: May 7, 2015
-		 Description: 
+		 Description: Two classes that show the differences between a regular class and one that uses generics. myCoolArray is the regular class, & genericArray uses generic types. 
 */
 
 
@@ -12,25 +12,31 @@ import java.util.Vector;
 
 /**
  * @author hugo
- *
+ * @note This class uses generics. The T inside the angle brackets <> is the type that is passed in the constructor. 
  */
-class genericArray <T> {
-	Vector<T> dataThings;
+class genericArray <T> {  // T is the datatype that will be passed to the class when making this object.
+	Vector<T> dataThings; // A Vector is like an array, but can be extended. (It is not a fixed length.) Notice how
+							// it is of type "T", the type that is passed when making the object.
+	
     public genericArray(){
-        dataThings = new Vector<T>();
+        dataThings = new Vector<T>(); //initialises the vector array with the passed datatype. 
     }
-    public T getFirstElement(){
+    
+    public T getFirstElement(){ //note that the return type is the type that is passed. 
         return dataThings.get(0);
     }
+    
     //etc. more functions to get/set indexes in the array
 }
 
-class myCoolArray {
-    int[] dataThings;
+class myCoolArray { //performs the same actions as genericArray, but can accept only one type. In this case, integers
+    int[] dataThings; //notice the datatype is "set in stone"
+    
     public myCoolArray(int length){
         dataThings = new int[length];
     }
-    public int getFirstElement(){
+    
+    public int getFirstElement(){ //the datatypes are all preset, and can't be set
         return dataThings[0];
     }
     //etc. more functions to get/set indexes in the array
